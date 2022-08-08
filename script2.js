8f2929772c7d107e93f97e115362b7104f7b44c7
Mayke Ken <devmayke@gmail.com>
2022-08-08 18:36:35 -0300

    testando o teste

--- script2.js
+++ script2.js
@@ -2,14 +2,13 @@ console.log("uhulllllllll")
 console.log("novo commit")
 
 
-
 function soma(a,b){
     return a+b
-
 }
+
 
  function multiplica(a,b){
     return a*b
-
 }
+
 module.exports= {soma, multiplica}
